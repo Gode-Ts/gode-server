22f7c5d10bd562312cd1ea93f0be60bb627f24f0
yasnielfajardo <yasnielfajardo@pormel.net>
2026-05-13 00:28:48 -0300

    Bump gopress static prefix path

--- cmd/gode/main.go
+++ cmd/gode/main.go
@@ -15,7 +15,7 @@ import (
 	"github.com/Gode-Ts/gode-server/internal/supervisor"
 )
 
-const version = "0.1.14"
+const version = "0.1.15"
 
 func main() {
 	os.Exit(run(os.Args[1:], os.Stdout, os.Stderr))
