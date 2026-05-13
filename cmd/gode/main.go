8896ff82648d6d123a51d13d048a102b9e0ab481
yasnielfajardo <yasnielfajardo@pormel.net>
2026-05-12 22:24:17 -0300

    Bump gopress dependency

--- cmd/gode/main.go
+++ cmd/gode/main.go
@@ -15,7 +15,7 @@ import (
 	"github.com/Gode-Ts/gode-server/internal/supervisor"
 )
 
-const version = "0.1.4"
+const version = "0.1.5"
 
 func main() {
 	os.Exit(run(os.Args[1:], os.Stdout, os.Stderr))
