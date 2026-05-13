17abc99d3eefdaf19322a46fd6dc114cb017d5de
yasnielfajardo <yasnielfajardo@pormel.net>
2026-05-12 23:47:56 -0300

    Bump gopress two param path

--- cmd/gode/main.go
+++ cmd/gode/main.go
@@ -15,7 +15,7 @@ import (
 	"github.com/Gode-Ts/gode-server/internal/supervisor"
 )
 
-const version = "0.1.9"
+const version = "0.1.10"
 
 func main() {
 	os.Exit(run(os.Args[1:], os.Stdout, os.Stderr))
